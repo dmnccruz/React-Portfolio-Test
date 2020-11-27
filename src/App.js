31381ade4fe5dee58a8af266adeb32fa82f5fa46
dominicmartincruz <dominicmartincruz@gmail.com>
2020-11-27 21:27:10 +0800

    init4

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import './App.css';
 import Landing from "./components/Landing";
 import FirstSection from "./components/FirstSection";
-import SecondSection from "./components/SecondSection";
+// import SecondSection from "./components/SecondSection";
 
 function App() {
   useEffect(() => {
